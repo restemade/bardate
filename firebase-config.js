7ca444032c30ac83be4e2511d28d58af1ac531d9
restemade <dimash.opg24@gmail.com>
2025-04-06 03:53:42 +0500

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,13 @@
-
-// Замените параметры ниже на свои из Firebase Console
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+var firebaseConfig = {
+  apiKey: "AIzaSyDHXzcyLhCwWzatyYVKgGutVrSR8_ST214",
+  authDomain: "bardate-6da25.firebaseapp.com",
+  databaseURL: "https://bardate-6da25-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bardate-6da25",
+  storageBucket: "bardate-6da25.firebasestorage.app",
+  messagingSenderId: "960943001047",
+  appId: "1:960943001047:web:9001cc2610f33939eb8f92",
+  measurementId: "G-PWR6F47LPJ"
 };
 
 firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
+var db = firebase.database();
